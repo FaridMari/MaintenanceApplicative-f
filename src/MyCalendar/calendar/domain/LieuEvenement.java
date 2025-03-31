9cbe61f515f52f4ed7f9b3a0108fceee5be08c8a
Mari Farid <farid.mari3@etu.univ-lorraine.fr>
2025-03-31 14:16:28 +0200

    création du Value object "LieuEvenement"

---
+++ src/MyCalendar/calendar/domain/LieuEvenement.java
@@ -0,0 +1,28 @@
+package MyCalendar.calendar.domain;
+
+import java.util.Objects;
+
+public class LieuEvenement {
+    private final String nom;
+
+    public LieuEvenement(String nom) {
+        this.nom = nom;
+    }
+
+    public String value() {
+        return nom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof LieuEvenement)) return false;
+        LieuEvenement le = (LieuEvenement) o;
+        return Objects.equals(nom, le.nom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom);
+    }
+}
